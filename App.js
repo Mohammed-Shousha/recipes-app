3bb666d9552ad6b268914d970e1f88ba7a1e06ef
Mohammed Shousha <mshousha29@gmail.com>
2023-11-15 20:50:35 +0200

    updating packages

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StatusBar, I18nManager } from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import * as Network from 'expo-network'
 import Main from './Pages/Main/Main'
 import Search from './Pages/Search/Search'
@@ -25,167 +25,190 @@ import SignIn from './Pages/Forms/SignIn'
 import Register from './Pages/Forms/Register'
 import Recipes from './Containers/Recipes'
 import { Icon } from './Components/Images'
-import Back from './Components/Back'
 import { Container } from './Components/Containers'
 import { Title } from './Components/Texts'
 import { DataProvider, RecipesProvider, DataContext } from './Data/Context'
-import { NavIcons, IconsSizes } from './Data/Database'
+import {
+  NavIcons,
+  IconsSizes,
+  tabOptions,
+  stackScreenOptions,
+  recipeScreenOptions,
+} from './Data/Database'
 import noConnection from './Data/images/noConnection.png'
 
 I18nManager.forceRTL(false)
 I18nManager.allowRTL(false)
 
 const client = new ApolloClient({
-   uri: "https://recipes-app-apollo-server.herokuapp.com/graphql",
-   // uri: "http://192.168.1.19:5000/graphql",
-   cache: new InMemoryCache()
+  uri: 'https://recipes-app-backend-plxd.onrender.com/graphql',
+  //   uri: "http://192.168.1.18:7000/graphql",
+  cache: new InMemoryCache(),
 })
 
 const Tab = createBottomTabNavigator()
 
-const screenOptions = {
-   headerStyle: {
-      backgroundColor: '#96fbc4',
-      height: 50,
-      borderBottomColor: '#96fbc4',
-   },
-   headerTitleStyle: {
-      fontSize: 28,
-      color: '#214151',
-      fontFamily: 'sans-serif-light',
-   },
-   headerTitleAlign: 'center',
-   headerBackImage: () => <Back />,
-}
-
-const recipeOptions = {
-   headerTitle: '',
-   headerTransparent: true,
-   headerBackImage: () => <Back recipe />
-}
-
 const MainStack = createStackNavigator()
 const MainStackScreen = () => (
-   <MainStack.Navigator screenOptions={screenOptions}>
-      <MainStack.Screen name="Main" component={Main} />
-      <MainStack.Screen name="Recipes" component={Recipes} />
-      <MainStack.Screen name="Recipe" component={RecipePage} options={recipeOptions} />
-      <MainStack.Screen name="Sign In" component={SignIn} />
-      <MainStack.Screen name="Register" component={Register} />
-   </MainStack.Navigator>
+  <MainStack.Navigator screenOptions={stackScreenOptions}>
+    <MainStack.Screen name="Main" component={Main} />
+    <MainStack.Screen name="Recipes" component={Recipes} />
+    <MainStack.Screen
+      name="Recipe"
+      component={RecipePage}
+      options={recipeScreenOptions}
+    />
+    <MainStack.Screen
+      name="Sign In"
+      component={SignIn}
+      // options={{ presentation: "modal" }}
+    />
+    <MainStack.Screen name="Register" component={Register} />
+  </MainStack.Navigator>
 )
 
 const SearchStack = createStackNavigator()
 const SearchStackScreen = () => (
-   <SearchStack.Navigator screenOptions={screenOptions}>
-      <SearchStack.Screen name='Search' component={Search} />
-      <SearchStack.Screen name='Ingredients' component={ByIngredients} />
-      <SearchStack.Screen name="Recipes" component={Recipes} />
-      <SearchStack.Screen name='Time' component={ByTime} />
-      <SearchStack.Screen name='Diet' component={ByDiet} />
-      <SearchStack.Screen name='Calories' component={ByCalories} />
-      <SearchStack.Screen name='Search Recipes' component={Recipes} options={{ headerTitle: 'Recipes' }} />
-      <SearchStack.Screen name='Search Recipe' component={RecipePage} options={recipeOptions} />
-      <SearchStack.Screen name="Recipe" component={RecipePage} options={recipeOptions} />
-   </SearchStack.Navigator>
+  <SearchStack.Navigator screenOptions={stackScreenOptions}>
+    <SearchStack.Screen name="Search" component={Search} />
+    <SearchStack.Screen name="Ingredients" component={ByIngredients} />
+    <SearchStack.Screen name="Recipes" component={Recipes} />
+    <SearchStack.Screen name="Time" component={ByTime} />
+    <SearchStack.Screen name="Diet" component={ByDiet} />
+    <SearchStack.Screen name="Calories" component={ByCalories} />
+    <SearchStack.Screen
+      name="Search Recipes"
+      component={Recipes}
+      options={{ headerTitle: 'Recipes' }}
+    />
+    <SearchStack.Screen
+      name="Search Recipe"
+      component={RecipePage}
+      options={recipeScreenOptions}
+    />
+    <SearchStack.Screen
+      name="Recipe"
+      component={RecipePage}
+      options={recipeScreenOptions}
+    />
+  </SearchStack.Navigator>
 )
 
 const FavouriteStack = createStackNavigator()
 const FavouriteStackScreen = () => {
-   const { isSignedIn } = useContext(DataContext)
-   return (
-      <FavouriteStack.Navigator screenOptions={screenOptions}>
-         {isSignedIn ?
-            <>
-               <FavouriteStack.Screen name='Favorite Recipes' component={Favourite} />
-               <FavouriteStack.Screen name="FavRecipe" component={RecipePage} options={recipeOptions} />
-            </>
-            :
-            <>
-               <FavouriteStack.Screen name="Profile" component={Profile} />
-               <FavouriteStack.Screen name="Sign In" component={SignIn} />
-               <FavouriteStack.Screen name="Register" component={Register} />
-            </>
-         }
-      </FavouriteStack.Navigator>
-   )
+  const { isSignedIn } = useContext(DataContext)
+  return (
+    <FavouriteStack.Navigator screenOptions={stackScreenOptions}>
+      {isSignedIn ? (
+        <>
+          <FavouriteStack.Screen
+            name="Favorite Recipes"
+            component={Favourite}
+          />
+          <FavouriteStack.Screen
+            name="FavRecipe"
+            component={RecipePage}
+            options={recipeScreenOptions}
+          />
+        </>
+      ) : (
+        <>
+          <FavouriteStack.Screen name="Profile" component={Profile} />
+          <FavouriteStack.Screen name="Sign In" component={SignIn} />
+          <FavouriteStack.Screen name="Register" component={Register} />
+        </>
+      )}
+    </FavouriteStack.Navigator>
+  )
 }
 
 const UserStack = createStackNavigator()
 const UserStackScreen = () => {
-   const { isSignedIn } = useContext(DataContext)
-   return (
-      <UserStack.Navigator screenOptions={screenOptions}>
-         {isSignedIn ?
-            <>
-               <UserStack.Screen name='User' component={User} />
-               <UserStack.Screen name='Edit Profile' component={EditProfile} />
-               <UserStack.Screen name='Change Password' component={ChangePassword} />
-               <UserStack.Screen name='Add Recipe' component={AddRecipe} />
-               <UserStack.Screen name='Edit Recipe' component={EditRecipe} />
-               <UserStack.Screen name="My Recipes" component={UserRecipes} />
-               <UserStack.Screen name="User Recipe" component={UserRecipePage} options={recipeOptions} />
-            </>
-            :
-            <>
-               <UserStack.Screen name='Profile' component={Profile} />
-               <UserStack.Screen name="Sign In" component={SignIn} />
-               <UserStack.Screen name="Register" component={Register} />
-            </>
-         }
-      </UserStack.Navigator>
-   )
+  const { isSignedIn } = useContext(DataContext)
+  return (
+    <UserStack.Navigator screenOptions={stackScreenOptions}>
+      {isSignedIn ? (
+        <>
+          <UserStack.Screen name="User" component={User} />
+          <UserStack.Screen name="Edit Profile" component={EditProfile} />
+          <UserStack.Screen name="Change Password" component={ChangePassword} />
+          <UserStack.Screen name="Add Recipe" component={AddRecipe} />
+          <UserStack.Screen name="Edit Recipe" component={EditRecipe} />
+          <UserStack.Screen name="My Recipes" component={UserRecipes} />
+          <UserStack.Screen
+            name="User Recipe"
+            component={UserRecipePage}
+            options={recipeScreenOptions}
+          />
+        </>
+      ) : (
+        <>
+          <UserStack.Screen name="Profile" component={Profile} />
+          <UserStack.Screen name="Sign In" component={SignIn} />
+          <UserStack.Screen name="Register" component={Register} />
+        </>
+      )}
+    </UserStack.Navigator>
+  )
 }
 
 const App = () => {
+  const [connected, setConnected] = useState(true)
 
-   const [connected, setConnected] = useState(true)
-
-   useEffect(() => {
-      const getConnectionStatus = async () => {
-         const result = await Network.getNetworkStateAsync()
-         if (!result.isInternetReachable) {
-            setConnected(false)
-         }
+  useEffect(() => {
+    const getConnectionStatus = async () => {
+      const result = await Network.getNetworkStateAsync()
+      if (!result.isInternetReachable) {
+        setConnected(false)
       }
-      getConnectionStatus()
-   })
+    }
+    getConnectionStatus()
+  })
 
-   return (
-      <>
-         {connected ?
-            <ApolloProvider client={client}>
-               <RecipesProvider>
-                  <DataProvider>
-                     <NavigationContainer>
-                        <Tab.Navigator initialRouteName="Main" tabBarOptions={{ showLabel: false, keyboardHidesTabBar: true, style: { backgroundColor: '#eff7e1', justifyContent: 'space-between' } }}
-                           screenOptions={({ route }) => ({
-                              tabBarIcon: ({ focused }) => {
-                                 const iconName = focused ? NavIcons[route.name][0] : NavIcons[route.name][1]
-                                 const size = IconsSizes[route.name]
-                                 return <Icon source={iconName} size={size} />
-                              }
-                           })}
-                        >
-                           <Tab.Screen name="Main" component={MainStackScreen} />
-                           <Tab.Screen name="Search" component={SearchStackScreen} />
-                           <Tab.Screen name='Favourite' component={FavouriteStackScreen} />
-                           <Tab.Screen name="User" component={UserStackScreen} />
-                        </Tab.Navigator>
-                        <StatusBar hidden={true} />
-                     </NavigationContainer>
-                  </DataProvider>
-               </RecipesProvider>
-            </ApolloProvider>
-            :
-            <Container center>
-               <Icon
-                  source={noConnection}
-                  size='100'
-               />
-               <Title>You are not connected to the internet, check your connection and try again.</Title>
-            </Container>}
-      </>
-   )
+  return (
+    <>
+      {connected ? (
+        <ApolloProvider client={client}>
+          <RecipesProvider>
+            <DataProvider>
+              <NavigationContainer>
+                <Tab.Navigator
+                  initialRouteName="Main-Tab"
+                  screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused }) => {
+                      const routeName = route.name.split('-')[0]
+                      const iconName = focused
+                        ? NavIcons[routeName][0]
+                        : NavIcons[routeName][1]
+                      const size = IconsSizes[routeName]
+                      return <Icon source={iconName} size={size} />
+                    },
+                    ...tabOptions,
+                  })}
+                >
+                  <Tab.Screen name="Main-Tab" component={MainStackScreen} />
+                  <Tab.Screen name="Search-Tab" component={SearchStackScreen} />
+                  <Tab.Screen
+                    name="Favourite-Tab"
+                    component={FavouriteStackScreen}
+                  />
+                  <Tab.Screen name="User-Tab" component={UserStackScreen} />
+                </Tab.Navigator>
+                <StatusBar hidden={true} />
+              </NavigationContainer>
+            </DataProvider>
+          </RecipesProvider>
+        </ApolloProvider>
+      ) : (
+        <Container center>
+          <Icon source={noConnection} size="100" />
+          <Title>
+            You are not connected to the internet, check your connection and try
+            again.
+          </Title>
+        </Container>
+      )}
+    </>
+  )
 }
 export default App
