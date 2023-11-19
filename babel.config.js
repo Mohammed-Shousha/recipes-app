3522fb45f01653b997d6757283c0de5306269b7a
Mohammed Shousha <mshousha29@gmail.com>
2023-11-19 23:33:17 +0200

    using aliases

--- babel.config.js
+++ babel.config.js
@@ -9,19 +9,41 @@
 // };
 
 module.exports = {
-   presets: ['module:metro-react-native-babel-preset'],
-   "plugins": [
-      [
-         "module:react-native-dotenv",
-         {
-            "moduleName": "@env",
-            "path": ".env",
-            "blacklist": null,
-            "whitelist": null,
-            "safe": false,
-            "allowUndefined": true
-         }
-      ]
-   ]
-};
-
+  presets: [
+    'module:metro-react-native-babel-preset',
+    [
+      'babel-preset-expo',
+      {
+        jsxRuntime: 'automatic',
+        importSource: 'react',
+      },
+    ],
+  ],
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
+      },
+    ],
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: ['.js', '.jsx', '.json'],
+        alias: {
+          '@root': './',
+          '@components': './components',
+          '@screens': './screens',
+          '@utils': './utils',
+          '@assets': './assets',
+        },
+      },
+    ],
+  ],
+}
