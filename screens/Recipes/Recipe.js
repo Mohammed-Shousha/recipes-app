f6c2f04044e13a3e49805084c3028c965cd16be1
Mohammed Shousha <mshousha29@gmail.com>
2023-12-06 07:58:53 +0200

    updating Recipe screens

--- screens/Recipes/Recipe.js
+++ screens/Recipes/Recipe.js
@@ -1,96 +1,65 @@
-import { useContext, useEffect, useState } from 'react'
-import { ScrollView, Modal, Pressable } from 'react-native'
+import { ToastAndroid } from 'react-native'
 import { useMutation } from '@apollo/client'
 
-import {
-  RecipeDetailsContainer,
-  RecipeDetail,
-  RecipeInfo,
-  RowContainer,
-  Container,
-  AlertContainer,
-} from '@components/styles/Containers.styles.'
-import {
-  RecipeImage,
-  PressableIcon,
-  Icon,
-  Exit,
-} from '@components/styles/Images.styles'
-import { RecipeTitle, Text } from '@components/styles/Texts.styles'
+import { RowContainer } from '@components/styles/Containers.styles'
+import { RecipeImage } from '@components/styles/Images.styles'
+import { RecipeTitle } from '@components/styles/Texts.styles'
 
-import { DataContext } from '@root/Context'
+import { useDataContext } from '@root/Context'
 
 import heart from '@assets/icons/greyHeart.png'
 import redHeart from '@assets/icons/redHeart.png'
-import recipeTypeImg from '@assets/icons/recipeType.png'
-import recipeTime from '@assets/icons/recipeTime.png'
-import recipeCalories from '@assets/icons/recipeCalories.png'
-import xImg from '@assets/icons/x.png'
 
-import { RECIPE_URL } from '@utils/constants'
-import { recipeDetails, recipesTypes } from '@utils/database'
 import {
   HANDLE_LIKING_RECIPE,
   HANDLE_UNLIKING_RECIPE,
 } from '@utils/graphql/mutations'
 
-import { LoadingDisplay } from '@components'
+import {
+  LoadingDisplay,
+  RecipeDetails,
+  RecipeSummary,
+  PressableIcon,
+} from '@components'
 
-export const Recipe = ({ route, navigation }) => {
+import useFetchRecipe from '@utils/hooks/useFetchRecipe'
+
+import { DEFAULT_RECIPE_IMAGE } from '@utils/constants'
+
+export const Recipe = ({ route }) => {
   const { id } = route.params
 
-  const { userData, setUserData, isSignedIn } = useContext(DataContext)
-  const { email, favRecipes } = userData
+  const {
+    userData: { email },
+    setUserData,
+    isSignedIn,
+  } = useDataContext()
 
-  const [loading, setLoading] = useState(false)
-  const [activeDetail, setActiveDetail] = useState(0)
-  const [like, setLike] = useState(false)
-  const [alert, setAlert] = useState(false)
+  const {
+    recipe,
+    instructions,
+    ingredients,
+    nutrients,
+    calories,
+    recipeType,
+    loading,
+    like,
+    setLike,
+  } = useFetchRecipe(id)
 
-  const showAlert = () => {
-    setAlert(true)
+  const recipeDetails = {
+    Ingredients: ingredients,
+    Directions: instructions,
+    Nutrients: nutrients,
   }
-
-  const hideAlert = () => {
-    setAlert(false)
-  }
-
-  const [recipe, setRecipe] = useState({})
-  const [nutrients, setNutrients] = useState([])
-  const [ingredients, setIngredients] = useState([])
-  const [instructions, setInstructions] = useState([])
-  const [calories, setCalories] = useState('')
-  const [recipeType, setRecipeType] = useState('')
-
-  const recipeInfo = [
-    {
-      name: recipeType,
-      color: '#ccf2f4',
-      image: recipeTypeImg,
-      size: 17,
-    },
-    {
-      name: recipe.readyInMinutes + ' m',
-      color: '#e8e9a1',
-      image: recipeTime,
-      size: 18,
-    },
-    {
-      name: Math.floor(calories) + ' kcal',
-      color: '#f1d1d0',
-      image: recipeCalories,
-      size: 18,
-    },
-  ]
 
   const [LikeRecipe] = useMutation(HANDLE_LIKING_RECIPE, {
     onCompleted({ LikeRecipe }) {
       if (LikeRecipe.result === 1) {
-        setUserData({
+        setUserData((userData) => ({
           ...userData,
           favRecipes: LikeRecipe.data,
-        })
-        setLike(true)
+        }))
       }
     },
   })
@@ -106,19 +75,19 @@ export const Recipe = ({ route, navigation }) => {
           image,
         },
       })
+      setLike(true)
     } else {
-      showAlert()
+      showToast()
     }
   }
 
   const [UnlikeRecipe] = useMutation(HANDLE_UNLIKING_RECIPE, {
     onCompleted({ UnlikeRecipe }) {
       if (UnlikeRecipe.result === 1) {
-        setUserData({
+        setUserData((userData) => ({
           ...userData,
           favRecipes: UnlikeRecipe.data,
-        })
-        setLike(false)
+        }))
       }
     },
   })
@@ -130,130 +99,45 @@ export const Recipe = ({ route, navigation }) => {
         id,
       },
     })
+    setLike(false)
   }
 
-  const redirect = (route) => {
-    navigation.navigate(route)
-    hideAlert()
-  }
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true)
-      const response = await fetch(RECIPE_URL(id))
-      const {
-        title,
-        image,
-        readyInMinutes,
-        nutrition,
-        extendedIngredients,
-        analyzedInstructions,
-        dishTypes,
-      } = await response.json()
-      setRecipe({ title, image, readyInMinutes })
-      setNutrients(nutrition.nutrients)
-      setCalories(nutrition.nutrients[0].amount)
-      setIngredients(extendedIngredients)
-      if (analyzedInstructions.length) {
-        setInstructions(analyzedInstructions[0].steps)
-      } else {
-        setInstructions([{ step: 'No Directions Available' }])
-      }
-      const filteredTypes = dishTypes
-        .filter((type) => recipesTypes.some((t) => t === type))
-        .map((t) => t.charAt(0).toUpperCase() + t.slice(1)) // Capitalize First Letter
-      setRecipeType(filteredTypes[0])
-      if (favRecipes) {
-        const liked = favRecipes.some((recipe) => recipe.id === +id) // id 'string', recipe.id 'int'
-        if (liked) {
-          setLike(true)
-        }
-      }
-      setLoading(false)
+  const toggleLike = () => {
+    if (like) {
+      unlikeRecipe()
+    } else {
+      likeRecipe(recipe)
     }
-    fetchData()
-  }, [id])
+  }
+
+  const showToast = () => {
+    ToastAndroid.show('Sign in or Resigter to like recipes', ToastAndroid.SHORT)
+  }
 
   if (loading) return <LoadingDisplay />
 
   return (
-    <Container>
-      <Modal
-        animationType="fade"
-        visible={alert}
-        transparent
-        onRequestClose={() => hideAlert()}
-      >
-        <AlertContainer>
-          <Exit onPress={() => hideAlert()}>
-            <Icon source={xImg} size="12" />
-          </Exit>
-          <RowContainer>
-            <Pressable onPress={() => redirect('Sign In')}>
-              <Text bold>Sign in</Text>
-            </Pressable>
-            <Text> or </Text>
-            <Pressable onPress={() => redirect('Register')}>
-              <Text bold>Register</Text>
-            </Pressable>
-            <Text> to Like Recipes</Text>
-          </RowContainer>
-        </AlertContainer>
-      </Modal>
+    <>
+      <RecipeImage source={{ uri: recipe.image ?? DEFAULT_RECIPE_IMAGE }} />
 
-      <RecipeImage source={{ uri: recipe.image }} />
-      <ScrollView>
-        <RowContainer>
-          <RecipeTitle>{recipe.title}</RecipeTitle>
-          <PressableIcon
-            onPress={!like ? () => likeRecipe(recipe) : unlikeRecipe}
-          >
-            <Icon source={like ? redHeart : heart} size="30" />
-          </PressableIcon>
-        </RowContainer>
-        <RowContainer>
-          {recipeInfo.map((r, i) => (
-            <RecipeInfo key={i} color={r.color}>
-              <Icon source={r.image} size={r.size} />
-              <Text size={r.size}>{r.name}</Text>
-            </RecipeInfo>
-          ))}
-        </RowContainer>
-        <RowContainer noPadding>
-          {recipeDetails.map((detail, i) => (
-            <RecipeDetail
-              onPress={() => setActiveDetail(i)}
-              active={activeDetail === i}
-              key={i}
-            >
-              <Text color="#214151">{detail}</Text>
-            </RecipeDetail>
-          ))}
-        </RowContainer>
-        {activeDetail === 0 ? (
-          <RecipeDetailsContainer>
-            {ingredients.map((ingredient, i) => (
-              <Text key={i} color="#393e46">
-                • {ingredient.original}
-              </Text>
-            ))}
-          </RecipeDetailsContainer>
-        ) : activeDetail === 1 ? (
-          <RecipeDetailsContainer>
-            {instructions.map((instruction, i) => (
-              <Text key={i}>• {instruction.step}</Text>
-            ))}
-          </RecipeDetailsContainer>
-        ) : (
-          <RecipeDetailsContainer>
-            {nutrients.map((nutrient, i) => (
-              <Text key={i}>
-                • {nutrient.name}: {nutrient.amount} {nutrient.unit}
-              </Text>
-            ))}
-          </RecipeDetailsContainer>
-        )}
-      </ScrollView>
-    </Container>
+      <RowContainer>
+        <RecipeTitle>{recipe.title}</RecipeTitle>
+        <PressableIcon
+          onPress={toggleLike}
+          icon={like ? redHeart : heart}
+          size="30"
+        />
+      </RowContainer>
+
+      <RecipeSummary
+        recipe={{
+          type: recipeType,
+          time: recipe.readyInMinutes,
+          calories,
+        }}
+      />
+
+      <RecipeDetails recipeDetails={recipeDetails} />
+    </>
   )
 }
--- screens/Recipes/UserRecipe.js
+++ screens/Recipes/UserRecipe.js
@@ -1,26 +1,12 @@
-import { useContext, useState } from 'react'
-import { Modal, ScrollView } from 'react-native'
+import { useState } from 'react'
 import { useMutation } from '@apollo/client'
 
-import {
-  RecipeDetailsContainer,
-  RecipeDetail,
-  RecipeInfo,
-  RowContainer,
-  Container,
-  ConfirmContainer,
-} from '@components/styles/Containers.styles.'
-import {
-  RecipeImage,
-  Icon,
-  PressableIcon,
-} from '@components/styles/Images.styles'
-import { RecipeTitle, Text } from '@components/styles/Texts.styles'
+import { RowContainer } from '@components/styles/Containers.styles'
+import { RecipeImage } from '@components/styles/Images.styles'
+import { RecipeTitle } from '@components/styles/Texts.styles'
 
-import { DataContext } from '@root/Context'
+import { useDataContext } from '@root/Context'
 
-import recipeTypeImg from '@assets/icons/recipeType.png'
-import recipeTime from '@assets/icons/recipeTime.png'
 import bin from '@assets/icons/bin.png'
 import edit from '@assets/icons/edit_2.png'
 
@@ -28,19 +14,23 @@ import { splitLines } from '@utils/helpers'
 import { HANDLE_DELETING_RECIPE } from '@utils/graphql/mutations'
 import { DEFAULT_RECIPE_IMAGE } from '@utils/constants'
 
-import { Button } from '@components'
+import {
+  RecipeDetails,
+  RecipeSummary,
+  PressableIcon,
+  ConfirmModal,
+} from '@components'
 
 export const UserRecipe = ({ route, navigation }) => {
   const {
     userData: { recipes, email },
     setUserData,
     setLoading,
-  } = useContext(DataContext)
+  } = useDataContext()
   const { id } = route.params
 
   const recipe = recipes.find((recipe) => recipe.id === id)
 
-  const [activeDetail, setActiveDetail] = useState(0)
   const [modal, setModal] = useState(false)
 
   const openModal = () => setModal(true)
@@ -49,22 +39,7 @@ export const UserRecipe = ({ route, navigation }) => {
   const ingredients = splitLines(recipe.ingredients)
   const directions = splitLines(recipe.directions)
 
-  const recipeInfo = [
-    {
-      name: recipe.type,
-      color: '#ccf2f4',
-      image: recipeTypeImg,
-      size: 21,
-    },
-    {
-      name: recipe.time + ' m',
-      color: '#e8e9a1',
-      image: recipeTime,
-      size: 20,
-    },
-  ]
-
-  const recipeDetails = ['Ingredients', 'Directions']
+  const recipeDetails = { Ingredients: ingredients, Directions: directions }
 
   const [DeleteRecipe] = useMutation(HANDLE_DELETING_RECIPE, {
     onCompleted({ DeleteRecipe }) {
@@ -92,90 +67,36 @@ export const UserRecipe = ({ route, navigation }) => {
     }, 500)
   }
 
+  const editRecipe = () => {
+    navigation.navigate('Edit Recipe', { id: recipe.id })
+  }
+
   return (
-    <Container>
-      <Modal
-        animationType="fade"
-        visible={modal}
-        transparent
-        onRequestClose={closeModal}
-      >
-        <ConfirmContainer>
-          <Text>Are you sure you want to delete this recipe?</Text>
-          <RowContainer>
-            <Button
-              onPress={deleteRecipe}
-              style={{ width: '45%', fontSize: '20px' }}
-            >
-              Confirm
-            </Button>
-            <Button
-              style={{ width: '45%', fontSize: '20px' }}
-              onPress={closeModal}
-              secondary
-            >
-              Cancel
-            </Button>
-          </RowContainer>
-        </ConfirmContainer>
-      </Modal>
+    <>
       <RecipeImage
         source={{
           uri: recipe.image ?? DEFAULT_RECIPE_IMAGE,
         }}
       />
-      <ScrollView>
-        <RowContainer width="95%">
-          <RecipeTitle>{recipe.title}</RecipeTitle>
-          <RowContainer width="20%">
-            <PressableIcon
-              onPress={() =>
-                navigation.navigate('Edit Recipe', { id: recipe.id })
-              }
-            >
-              <Icon source={edit} size="30" />
-            </PressableIcon>
-            <PressableIcon onPress={openModal}>
-              <Icon source={bin} size="30" />
-            </PressableIcon>
-          </RowContainer>
+
+      <RowContainer style={{ width: '95%' }}>
+        <RecipeTitle>{recipe.title}</RecipeTitle>
+        <RowContainer style={{ width: '20%' }}>
+          <PressableIcon onPress={editRecipe} icon={edit} size="30" />
+          <PressableIcon onPress={openModal} icon={bin} size="30" />
         </RowContainer>
-        <RowContainer>
-          {recipeInfo.map((r, i) => (
-            <RecipeInfo user key={i} color={r.color}>
-              <Icon source={r.image} size={r.size} />
-              <Text size={r.size}>{r.name}</Text>
-            </RecipeInfo>
-          ))}
-        </RowContainer>
-        <RowContainer noPadding>
-          {recipeDetails.map((detail, i) => (
-            <RecipeDetail
-              user
-              onPress={() => setActiveDetail(i)}
-              active={activeDetail === i}
-              key={i}
-            >
-              <Text color="#214151">{detail}</Text>
-            </RecipeDetail>
-          ))}
-        </RowContainer>
-        {activeDetail === 0 ? (
-          <RecipeDetailsContainer>
-            {ingredients.map((ingredient, i) => (
-              <Text key={i} color="#393e46">
-                • {ingredient}
-              </Text>
-            ))}
-          </RecipeDetailsContainer>
-        ) : (
-          <RecipeDetailsContainer>
-            {directions.map((direction, i) => (
-              <Text key={i}>• {direction}</Text>
-            ))}
-          </RecipeDetailsContainer>
-        )}
-      </ScrollView>
-    </Container>
+      </RowContainer>
+
+      <RecipeSummary recipe={recipe} />
+
+      <RecipeDetails recipeDetails={recipeDetails} />
+
+      <ConfirmModal
+        message="Are you sure you want to delete this recipe?"
+        visible={modal}
+        onConfirm={deleteRecipe}
+        onClose={closeModal}
+      />
+    </>
   )
 }
