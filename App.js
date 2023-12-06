6e52985f6411195d7db106e1bd5969c6d4c3e680
Mohammed Shousha <mshousha29@gmail.com>
2023-12-06 07:47:28 +0200

    using HOC

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { StatusBar, I18nManager } from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { NavigationContainer } from '@react-navigation/native'
@@ -13,23 +13,22 @@ import {
   TimeSearch,
   CaloriesSearch,
   Recipe,
+  Recipes,
   UserRecipe,
   Favourite,
   User,
   UserRecipes,
   EditProfile,
   ChangePassword,
-  AddRecipe,
-  EditRecipe,
+  RecipeForm,
   Profile,
   SignIn,
   Register,
 } from '@screens'
 
-import Recipes from '@root/screens/Recipes/Recipes'
 import { Icon } from '@components/styles/Images.styles'
 
-import { DataProvider, RecipesProvider, DataContext } from '@root/Context'
+import { DataProvider, RecipesProvider, useDataContext } from '@root/Context'
 
 import {
   navIcons,
@@ -37,58 +36,61 @@ import {
   tabOptions,
   stackScreenOptions,
   recipeScreenOptions,
-} from '@root/utils/database'
+} from '@utils/database'
 
 import noConnection from '@assets/images/noConnection.png'
 import { ErrorDisplay } from '@components'
+
+import { BACKEND_URL } from '@utils/constants'
+import { Container } from '@components/styles/Containers.styles'
 
 I18nManager.forceRTL(false)
 I18nManager.allowRTL(false)
 
 const client = new ApolloClient({
-  uri: 'https://recipes-app-backend-plxd.onrender.com/graphql',
+  uri: BACKEND_URL,
   //   uri: "http://192.168.1.18:7000/graphql",
   cache: new InMemoryCache(),
 })
+
+const withContainer = (Component) => (props) => (
+  <Container>
+    <Component {...props} />
+  </Container>
+)
 
 const Tab = createBottomTabNavigator()
 
 const MainStack = createStackNavigator()
 const MainStackScreen = () => (
   <MainStack.Navigator screenOptions={stackScreenOptions}>
-    <MainStack.Screen name="Main" component={Main} />
-    <MainStack.Screen name="Recipes" component={Recipes} />
+    <MainStack.Screen name="Main" component={withContainer(Main)} />
+    <MainStack.Screen name="Recipes" component={withContainer(Recipes)} />
     <MainStack.Screen
       name="Recipe"
-      component={Recipe}
+      component={withContainer(Recipe)}
       options={recipeScreenOptions}
     />
-    <MainStack.Screen
-      name="Sign In"
-      component={SignIn}
-      // options={{ presentation: "modal" }}
-    />
-    <MainStack.Screen name="Register" component={Register} />
+    <MainStack.Screen name="Sign In" component={withContainer(SignIn)} />
+    <MainStack.Screen name="Register" component={withContainer(Register)} />
   </MainStack.Navigator>
 )
 
 const SearchStack = createStackNavigator()
 const SearchStackScreen = () => (
   <SearchStack.Navigator screenOptions={stackScreenOptions}>
-    <SearchStack.Screen name="Search" component={Search} />
+    <SearchStack.Screen name="Search" component={withContainer(Search)} />
     <SearchStack.Screen name="Ingredients" component={IngredientsSearch} />
-    <SearchStack.Screen name="Recipes" component={Recipes} />
-    <SearchStack.Screen name="Time" component={TimeSearch} />
-    <SearchStack.Screen name="Diet" component={DietSearch} />
-    <SearchStack.Screen name="Calories" component={CaloriesSearch} />
+    <SearchStack.Screen name="Recipes" component={withContainer(Recipes)} />
+    <SearchStack.Screen name="Time" component={withContainer(TimeSearch)} />
+    <SearchStack.Screen name="Diet" component={withContainer(DietSearch)} />
     <SearchStack.Screen
-      name="Search Recipe"
-      component={Recipe}
-      options={recipeScreenOptions}
+      name="Calories"
+      component={withContainer(CaloriesSearch)}
     />
     <SearchStack.Screen
       name="Recipe"
-      component={Recipe}
+      component={withContainer(Recipe)}
       options={recipeScreenOptions}
     />
   </SearchStack.Navigator>
@@ -96,26 +98,35 @@ const SearchStackScreen = () => (
 
 const FavouriteStack = createStackNavigator()
 const FavouriteStackScreen = () => {
-  const { isSignedIn } = useContext(DataContext)
+  const { isSignedIn } = useDataContext()
   return (
     <FavouriteStack.Navigator screenOptions={stackScreenOptions}>
       {isSignedIn ? (
         <>
           <FavouriteStack.Screen
             name="Favourite Recipes"
-            component={Favourite}
+            component={withContainer(Favourite)}
           />
           <FavouriteStack.Screen
             name="Recipe"
-            component={Recipe}
+            component={withContainer(Recipe)}
             options={recipeScreenOptions}
           />
         </>
       ) : (
         <>
-          <FavouriteStack.Screen name="Profile" component={Profile} />
-          <FavouriteStack.Screen name="Sign In" component={SignIn} />
-          <FavouriteStack.Screen name="Register" component={Register} />
+          <FavouriteStack.Screen
+            name="Profile"
+            component={withContainer(Profile)}
+          />
+          <FavouriteStack.Screen
+            name="Sign In"
+            component={withContainer(SignIn)}
+          />
+          <FavouriteStack.Screen
+            name="Register"
+            component={withContainer(Register)}
+          />
         </>
       )}
     </FavouriteStack.Navigator>
@@ -124,28 +135,46 @@ const FavouriteStackScreen = () => {
 
 const UserStack = createStackNavigator()
 const UserStackScreen = () => {
-  const { isSignedIn } = useContext(DataContext)
+  const { isSignedIn } = useDataContext()
   return (
     <UserStack.Navigator screenOptions={stackScreenOptions}>
       {isSignedIn ? (
         <>
-          <UserStack.Screen name="User" component={User} />
-          <UserStack.Screen name="Edit Profile" component={EditProfile} />
-          <UserStack.Screen name="Change Password" component={ChangePassword} />
-          <UserStack.Screen name="Add Recipe" component={AddRecipe} />
-          <UserStack.Screen name="Edit Recipe" component={EditRecipe} />
-          <UserStack.Screen name="My Recipes" component={UserRecipes} />
+          <UserStack.Screen name="User" component={withContainer(User)} />
+          <UserStack.Screen
+            name="Edit Profile"
+            component={withContainer(EditProfile)}
+          />
+          <UserStack.Screen
+            name="Change Password"
+            component={withContainer(ChangePassword)}
+          />
+          <UserStack.Screen
+            name="Add Recipe"
+            component={withContainer(RecipeForm)}
+          />
+          <UserStack.Screen
+            name="Edit Recipe"
+            component={withContainer(RecipeForm)}
+          />
+          <UserStack.Screen
+            name="My Recipes"
+            component={withContainer(UserRecipes)}
+          />
           <UserStack.Screen
             name="User Recipe"
-            component={UserRecipe}
+            component={withContainer(UserRecipe)}
             options={recipeScreenOptions}
           />
         </>
       ) : (
         <>
-          <UserStack.Screen name="Profile" component={Profile} />
-          <UserStack.Screen name="Sign In" component={SignIn} />
-          <UserStack.Screen name="Register" component={Register} />
+          <UserStack.Screen name="Profile" component={withContainer(Profile)} />
+          <UserStack.Screen name="Sign In" component={withContainer(SignIn)} />
+          <UserStack.Screen
+            name="Register"
+            component={withContainer(Register)}
+          />
         </>
       )}
     </UserStack.Navigator>
@@ -165,7 +194,15 @@ const App = () => {
     getConnectionStatus()
   })
 
-  return connected ? (
+  if (!connected) {
+    return withContainer(ErrorDisplay)({
+      message:
+        'You are not connected to the internet, check your connection and try again.',
+      icon: noConnection,
+    })
+  }
+
+  return (
     <ApolloProvider client={client}>
       <RecipesProvider>
         <DataProvider>
@@ -197,11 +234,6 @@ const App = () => {
         </DataProvider>
       </RecipesProvider>
     </ApolloProvider>
-  ) : (
-    <ErrorDisplay
-      message="You are not connected to the internet, check your connection and try again."
-      icon={noConnection}
-    />
   )
 }
 export default App
